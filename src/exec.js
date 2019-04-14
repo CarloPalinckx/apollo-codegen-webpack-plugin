a5a1216cf996a43d9710d4de858149f7e939f84b
Carlo Palinckx <carlopalinckx@gmail.com>
2019-04-14 22:50:11 +0200

    Fixed race condition and increased performance significally (#3)

---
+++ src/exec.js
@@ -0,0 +1,33 @@
+const ora = require('ora');
+const cp = require('child_process');
+const path = require('path');
+
+const exec = (name, command) => {
+    const spinner = ora(`[ApolloWebpackPlugin] ${name}`).start();
+    const apollo = path.join(__dirname, '..', 'node_modules', '.bin', 'apollo');
+
+    return new Promise((resolve, reject) => {
+        cp.exec(`${apollo} ${command}`, (error, stdout, stderr) => {
+            if (error) {
+                spinner.fail();
+                console.error(error);
+                return reject();
+            }
+
+            if (stderr) {
+                spinner.fail();
+                console.error(stderr);
+                return reject();
+            }
+
+            if (stdout) {
+                console.log(stdout);
+            }
+
+            spinner.succeed();
+            resolve();
+        });
+    });
+};
+
+module.exports = exec;
---
+++ src/fetchSchema.js
@@ -0,0 +1,18 @@
+const exec = require('./exec');
+
+const fetchSchema = options => {
+    const { endpoint, localSchemaFile, config, header, tag, skipSSLValidation, key } = options;
+    const command = ['service:download'];
+
+    if (localSchemaFile) command.push(localSchemaFile);
+    if (config) command.push(`-c="${config}"`);
+    if (header) command.push(`--header="${header}"`);
+    if (endpoint) command.push(`--endpoint="${endpoint}"`);
+    if (tag) command.push(`-t="${tag}"`);
+    if (skipSSLValidation) command.push(`--skipSSLValidation`);
+    if (key) command.push(`--key="${key}"`);
+
+    return exec('Downloading schema', command.join(' '));
+};
+
+module.exports = fetchSchema;
