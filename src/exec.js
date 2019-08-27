68df3bb0d6ba6b69753c4df88fedc6ee1b704e72
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-08-27 10:14:42 +0200

    Cleaned up console output

--- src/exec.js
+++ src/exec.js
@@ -1,30 +1,30 @@
 const ora = require('ora');
 const cp = require('child_process');
 const path = require('path');
+const chalk = require('chalk');
 
 const exec = (name, command) => {
-    const spinner = ora(`[ApolloWebpackPlugin] ${name}\n`).start();
+    const spinner = ora(name).start();
     const apollo = path.join(__dirname, '..', 'node_modules', '.bin', 'apollo');
 
     return new Promise((resolve, reject) => {
         cp.exec(`${apollo} ${command}`, (error, stdout, stderr) => {
             if (error) {
-                spinner.fail();
-                console.error(error);
+                spinner.fail(`${name}\n${chalk.dim(error)}`);
                 return reject();
             }
 
-            if (stderr) {
-                spinner.fail();
-                console.error(stderr);
+            if (stderr && stderr.indexOf('Warning: apollo update available') === -1) {
+                spinner.fail(`${name}\n${chalk.dim(stderr)}`);
                 return reject();
             }
 
             if (stdout) {
-                console.log(stdout);
+                spinner.succeed(`${name}\n${chalk.dim(stdout)}`);
+            } else {
+                spinner.succeed();
             }
 
-            spinner.succeed();
             resolve();
         });
     });
